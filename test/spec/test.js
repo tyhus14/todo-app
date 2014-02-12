3a8d30d56525393a48c296930d7f78fd43e073c9
tyhus14 <huston.tyler3@gmail.com>
2014-02-12 18:19:09 -0500

    updated header and deleted certain things

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,10 +3,41 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    describe('In the Todo App', function () {
+            describe('when the add button is clicked', function () {
+            it('should push info to the array', function () {
 
+            	var todoArray = [];
+
+            	var todo = {
+            		description: "Hello",
+            		done: false,
+            		id: 4
+            	}
+
+            	$('.js-add-btn').click();
+
+            	todoArray.push(todo);
+
+            	expect(todoArray).to.contain(todo);
+            });
+        });
+
+            describe('when the delete button is clicked', function () {
+            it('should remove info in the array', function () {
+
+            	var todoArray = [
+
+            	{description: "Hello", done: false, id: 1},
+            	{description: "Hi", done: false, id: 2}
+
+            	];
+
+            	$('.js-add-btn').click();
+
+            	$(todoArray).remove([0]);
+
+            	expect(todoArray).to.deep.equal({description: "Hi", done: false, id: 2});
             });
         });
     });
